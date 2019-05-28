87887684dff0e6c6114977ce178fd9bf447d6f5b
Niladri Kar <niladri@Niladris-MacBook-Air.local>
2019-05-29 00:06:14 +0700

    initializing

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "sparkProject1"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+val sparkVersion = "2.0.0"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion
+)
---
+++ src/main/scala/Boot.scala
@@ -0,0 +1,32 @@
+import org.apache.spark.sql._
+
+object Boot extends App {
+
+  case class Index(row: Int, col: Int)
+
+  private val APP_NAME = "Spark Project 1"
+  private val PATH = "data/data1.csv"
+  private val MASTER = "local[*]"
+
+  println("hello")
+
+  val spark = SparkSession.builder.appName(APP_NAME).master(MASTER).getOrCreate
+
+  // Read file
+  //  val data = spark.read.format("csv").load(PATH)
+
+  val data = spark.read.option("inferSchema", true).csv(PATH).collect
+
+  val dataSeq = data.map(row => Array(row.getInt(0), row.getInt(1), row.getInt(2), row.getInt(3), row.getInt(4)))
+
+  val a = for {
+    i <- 0 until dataSeq.length
+    j <- 0 until dataSeq(i).length
+    if (dataSeq(i)(j) <= 40)
+  } yield Index(i, j) -> dataSeq(i)(j)
+
+  println(a.length)
+  println(a.toMap)
+
+
+}
